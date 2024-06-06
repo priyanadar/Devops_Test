81a8b27c742b3a971668f4e1563cc5c1fdfe8ded
priyanadar2000 <labuser@ip-172-31-5-151.ap-south-1.compute.internal>
2024-06-06 15:51:57 +0000

    adding same function from main branch

--- file_1.c
+++ file_1.c
@@ -3,7 +3,7 @@ int main() {
 
     int number1, number2, sum;
     
-    printf("Enter two integers: ");
+    printf("Enter two integers a & b: ");
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
